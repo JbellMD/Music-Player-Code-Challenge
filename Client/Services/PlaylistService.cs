9e5d7bc6e94a4e877109a79a0735473365c98438
Jonathan Bello <ijbello29@gmail.com>
2025-01-25 05:46:33 -0500

    Deployment changes

--- Client/Services/PlaylistService.cs
+++ Client/Services/PlaylistService.cs
@@ -3,18 +3,6 @@ using System.Net.Http.Json;
 
 namespace music_manager_starter.Client.Services
 {
-    public interface IPlaylistService
-    {
-        Task<IEnumerable<Playlist>> GetPlaylistsAsync();
-        Task<Playlist> GetPlaylistAsync(int id);
-        Task<Playlist> CreatePlaylistAsync(Playlist playlist);
-        Task<Playlist> UpdatePlaylistAsync(Playlist playlist);
-        Task DeletePlaylistAsync(int id);
-        Task AddSongToPlaylistAsync(int playlistId, int songId);
-        Task RemoveSongFromPlaylistAsync(int playlistId, int songId);
-        Task ReorderPlaylistSongsAsync(int playlistId, List<PlaylistSong> songs);
-    }
-
     public class PlaylistService : IPlaylistService
     {
         private readonly HttpService _httpService;
