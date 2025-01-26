fe0c28357fca01027e32d2539b1ea36ad7101602
Jonathan Bello <ijbello29@gmail.com>
2025-01-26 07:37:16 -0500

    Added MudPopoverProvider to App.razor with DisablePopoverProvider set to true Updated MudBlazor services configuration in Program.cs: Added configuration to disable throwing on duplicate providers Set PopoverOptions.ThrowOnDuplicateProvider to false Also improved the theme configuration in App.razor: Added explicit color values for better consistency Set primary and secondary colors to match Material Design

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using music_manager_starter.Client;
 using music_manager_starter.Client.Services;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -17,7 +18,10 @@ builder.Services.AddScoped<ISongService, SongService>();
 builder.Services.AddScoped<IPlaylistService, PlaylistService>();
 builder.Services.AddScoped<INotificationService, NotificationService>();
 
-// Add MudBlazor
-builder.Services.AddMudServices();
+// Add MudBlazor with configuration
+builder.Services.AddMudServices(config =>
+{
+    config.PopoverOptions.ThrowOnDuplicateProvider = false;
+});
 
 await builder.Build().RunAsync();
